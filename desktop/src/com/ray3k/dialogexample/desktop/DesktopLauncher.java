61793bda759c3c23f434488f06b236b8d7d45e28
Raymond Buckley <raymond.ray3k@gmail.com>
2017-09-21 09:13:06 -0700

    Project setup and added base UI graphics.

---
+++ desktop/src/com/ray3k/dialogexample/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ray3k.dialogexample.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ray3k.dialogexample.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
