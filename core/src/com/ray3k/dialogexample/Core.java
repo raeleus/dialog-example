42cab5dc35114d8e2792b033d7c0d215fa76b165
Raymond Buckley <raymond.ray3k@gmail.com>
2017-09-21 19:42:26 -0700

    Created UI skin files. Added basic stage code.

--- core/src/com/ray3k/dialogexample/Core.java
+++ core/src/com/ray3k/dialogexample/Core.java
@@ -3,20 +3,47 @@ package com.ray3k.dialogexample;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Core extends ApplicationAdapter {
-
+    private Skin skin;
+    private Stage stage;
+    
     @Override
     public void create() {
+        skin = new Skin(Gdx.files.internal("Dialog Example.json"));
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        
+        Table root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+        
+        Label label = new Label("Press R to login", skin, "red");
+        root.add(label).expand().top().left().pad(20.0f);
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void dispose() {
+        skin.dispose();
+        stage.dispose();
     }
 }
