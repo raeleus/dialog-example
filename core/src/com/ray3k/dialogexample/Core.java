7a0044a85fe06d50433f340921cdb36a987ae429
Raymond Buckley <raymond.ray3k@gmail.com>
2017-09-21 19:53:34 -0700

    Created an InputManager class and added it to an InputMultiplexer.

--- core/src/com/ray3k/dialogexample/Core.java
+++ core/src/com/ray3k/dialogexample/Core.java
@@ -1,7 +1,31 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Raymond Buckley.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.ray3k.dialogexample;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -12,12 +36,16 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class Core extends ApplicationAdapter {
     private Skin skin;
     private Stage stage;
+    private static Core core;
     
     @Override
     public void create() {
+        core = this;
+        InputManager inputManager = new InputManager();
+        
         skin = new Skin(Gdx.files.internal("Dialog Example.json"));
         stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(new InputMultiplexer(stage, inputManager));
         
         Table root = new Table();
         root.setFillParent(true);
@@ -45,5 +73,9 @@ public class Core extends ApplicationAdapter {
     public void dispose() {
         skin.dispose();
         stage.dispose();
+    }
+
+    public static Core getCore() {
+        return core;
     }
 }
